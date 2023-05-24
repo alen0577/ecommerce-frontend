1f729f686348065b8b26acb9e0e999e99082823a
Alen Antony <alenantony32@gmail.com>
2023-05-24 20:36:04 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')   
+
+def home(request):
+    return render(request,'home.html')
+         
+    
